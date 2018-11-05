fe57a28dd90085b6105bd8d5a4b3599442630bc9
Ryan <ryanschnarre@gmail.com>
2018-11-04 20:37:45 -0500

    First commit

---
+++ paymerang/README.md
@@ -0,0 +1,2 @@
+﻿This is an ASP.NET MVC application built with Visual Studio Community Edition that utilizes Angular JavaScript to update HTML tables with local JSON data in an asynchronous fashion
+
---
+++ paymerang/wwwroot/js/site.js
@@ -0,0 +1,201 @@
+﻿// Init app
+var app = angular.module("myApp", []);
+
+// HTML controller
+app.controller("myCtrl", function($scope, $http) {
+
+
+    // Initial load to tables
+        $http({
+            method: 'get', 
+            url: '/JSON/Info.json'
+        }).then(function (response) {
+            $scope.info = response.data[0];
+        },function (error){
+            console.log(error, 'can not get data.');
+        });
+        
+    // Scope update functions to tables and respective JSON objects
+
+        $scope.bleendot = function () {
+        // Load JSON data to $scope
+            $http({
+                method: 'get', 
+                url: '/JSON/Info.json'
+            }).then(function (response) {
+                $scope.info = response.data[0];
+            },function (error){
+                console.log(error, 'can not get data.');
+            });
+        }
+
+        $scope.farmage = function () {
+           // Load JSON data to $scope
+           $http({
+                method: 'get', 
+                url: '/JSON/Info.json'
+            }).then(function (response) {
+                $scope.info = response.data[1];    
+            },function (error){
+                console.log(error, 'can not get data.');
+            });
+        }
+
+        $scope.digirang = function () {
+        // Load JSON data to $scope
+            $http({
+                method: 'get', 
+                url: '/JSON/Info.json'
+            }).then(function (response) {
+                $scope.info = response.data[2];
+            },function (error){
+                console.log(error, 'can not get data.');
+            });
+        }
+
+        $scope.earthmark = function () {
+        // Load JSON data to $scope
+            $http({
+                method: 'get', 
+                url: '/JSON/Info.json'
+            }).then(function (response) {
+                $scope.info = response.data[3];
+            },function (error){
+                console.log(error, 'can not get data.');
+            });
+        }
+
+        $scope.datagen = function () {
+        // Load JSON data to $scope
+            $http({
+                method: 'get', 
+                url: '/JSON/Info.json'
+            }).then(function (response) {
+                $scope.info = response.data[4];
+            },function (error){
+                console.log(error, 'can not get data.');
+            });
+        }
+
+        $scope.twiggery = function () {
+        // Load JSON data to $scope
+            $http({
+                method: 'get', 
+                url: '/JSON/Info.json'
+            }).then(function (response) {
+                $scope.info = response.data[5];
+            },function (error){
+                console.log(error, 'can not get data.');
+            });
+        }
+
+        $scope.zolar = function () {
+        // Load JSON data to $scope
+            $http({
+                method: 'get', 
+                url: '/JSON/Info.json'
+            }).then(function (response) {
+                $scope.info = response.data[6];
+            },function (error){
+                console.log(error, 'can not get data.');
+            });
+        }
+
+        $scope.illumity = function () {
+        // Load JSON data to $scope
+            $http({
+                method: 'get', 
+                url: '/JSON/Info.json'
+            }).then(function (response) {
+                $scope.info = response.data[7];
+            },function (error){
+                console.log(error, 'can not get data.');
+            });
+        }
+
+        $scope.mazuda = function () {
+        // Load JSON data to $scope
+            $http({
+                method: 'get', 
+                url: '/JSON/Info.json'
+            }).then(function (response) {
+                $scope.info = response.data[8];
+            },function (error){
+                console.log(error, 'can not get data.');
+            });
+        }
+
+        $scope.zensure = function () {
+        // Load JSON data to $scope
+            $http({
+                method: 'get', 
+                url: '/JSON/Info.json'
+            }).then(function (response) {
+                $scope.info = response.data[9];
+            },function (error){
+                console.log(error, 'can not get data.');
+            });
+        }
+
+        $scope.buzzmaker = function () {
+        // Load JSON data to $scope
+            $http({
+                method: 'get', 
+                url: '/JSON/Info.json'
+            }).then(function (response) {
+                $scope.info = response.data[10];
+            },function (error){
+                console.log(error, 'can not get data.');
+            });
+        }
+
+        $scope.kangle = function () {
+        // Load JSON data to $scope
+            $http({
+                method: 'get', 
+                url: '/JSON/Info.json'
+            }).then(function (response) {
+                $scope.info = response.data[11];
+            },function (error){
+                console.log(error, 'can not get data.');
+            });
+        }
+
+         $scope.miraclis = function () {
+        // Load JSON data to $scope
+            $http({
+                method: 'get', 
+                url: '/JSON/Info.json'
+            }).then(function (response) {
+                $scope.info = response.data[12];
+            },function (error){
+                console.log(error, 'can not get data.');
+            });
+        }
+
+         $scope.sportan = function () {
+        // Load JSON data to $scope
+            $http({
+                method: 'get', 
+                url: '/JSON/Info.json'
+            }).then(function (response) {
+                $scope.info = response.data[13];
+            },function (error){
+                console.log(error, 'can not get data.');
+            });
+        }
+
+         $scope.manufact = function () {
+        // Load JSON data to $scope
+            $http({
+                method: 'get', 
+                url: '/JSON/Info.json'
+            }).then(function (response) {
+                $scope.info = response.data[14];
+            },function (error){
+                console.log(error, 'can not get data.');
+            });
+        }
+
+});
+
